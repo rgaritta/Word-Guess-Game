6f08a96954f2a4524394a7e35bd943483f0aa3ac
rgaritta <fender8@bellsouth.net>
2018-06-02 03:39:37 -0400

    word guess game push 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,27 @@
 //global variables
-var word = "the word";
+var word = "new york";
 var userGuessList = [];
-var wordState = [word.length];
+var wordState = [];
+wordState.length = word.length;
 var guessesLeft = 6;
+var wins = 0;
+
+console.log(wordState);
 
 
 
 function initWordState() {
 
     var html = "";
-    for (var i = 0; i < word.length; i++) {
-        html += "_ ";
+    for (var i = 0; i < wordState.length; i++) {
+        if (word[i] != " ") {
+            wordState[i] = "_ ";
+            html += "_ ";
+        }
+        else {
+            wordState[i] = " ";
+            html += "&nbsp;";
+        }
     }
     return html;
 }
@@ -20,7 +31,9 @@ document.getElementById("word").innerHTML = initWordState();
 
 document.onkeyup = function (event) {
 
-    var userGuess = event.key;
+    var userGuess = event.key.toLowerCase();
+
+    
 
     //Put letter in wordState array if it's in word and check to see if letter has been guessed, return 0 if letter isn't in word
     function checkLetter() {
@@ -40,19 +53,19 @@ document.onkeyup = function (event) {
         }
 
         for (var i = 0; i < userGuessList.length; i++) {
-            if (" " + userGuess == userGuessList[i]) {
+            if (" " + userGuess.toUpperCase() == userGuessList[i]) {
                 alert("You already guessed this letter");
                 return null;
             }
         }
 
-        userGuessList.push(" " + userGuess);
+        userGuessList.push(" " + userGuess.toUpperCase());
         return 0;
     }
 
     //call checkLetter function & check to see if the letter is in the word
     if (checkLetter() == 0) {
-        alert("Letter is not in word");
+        //alert("Letter is not in word");
         guessesLeft--;
         document.getElementById("guesses").innerHTML = "Incorrect guesses: " + userGuessList;
 
@@ -78,26 +91,47 @@ document.onkeyup = function (event) {
     }
 }
 
+//convert wordState array to printable string
+var printStatus = "";
+for (var i = 0; i < wordState.length; i++) {
+    if (wordState[i] == " ") {
+    printStatus += wordState[i] + "&nbsp;";
+    }
+    else {
+        printStatus += wordState[i] + " ";
+    }
+}
+document.getElementById("word").innerHTML = printStatus;
 
 
 
+//convert wordState to string userWord to check against chosen word or phrase
 var userWord = "";
 for (var i = 0; i < wordState.length; i++) {
     userWord += wordState[i];
 }
 
+document.getElementById("win-lose").innerHTML = "";
+
+//user wins if userWord equals the word or phrase
 if (userWord == word) {
-    document.write("you win!");
+    document.getElementById("win-lose").innerHTML = "You win! =)";
+    wins++;
+    
 }
 
+//user loses if they guessed 6 times
 if (guessesLeft == 0) {
-    document.write("you lose.");
+    document.getElementById("win-lose").innerHTML = "You lose. =(";
+    
 }
 
+document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft;
 
 
-console.log(userWord);
-console.log(wordState);
+
+//console.log(userWord);
+//console.log(wordState);
 
 
-};
+}
